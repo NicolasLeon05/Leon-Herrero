0145be1f22c7f7c64b0035da84b53f67dcf0e0e1
4rleco <117652001+4rleco@users.noreply.github.com>
2025-08-20 15:32:07 -0300

    Add vertex input

--- src/BaseGame.cpp
+++ src/BaseGame.cpp
@@ -1,3 +1,4 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include "Window.h"
@@ -20,6 +21,20 @@ int main(void)
     }
 
     renderer.MakeContextCurrent(window);
+
+    glewInit();
+
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), 
+        vertices, GL_STREAM_DRAW);
 
     while (!window.ShouldClose())
     {
