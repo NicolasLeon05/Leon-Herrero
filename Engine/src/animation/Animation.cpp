603b8bcdc33912c47c8f17bf54320e3e440ca176
4rleco <117652001+4rleco@users.noreply.github.com>
2025-11-18 15:58:01 -0300

    Fix AddFrame

--- Engine/src/animation/Animation.cpp
+++ Engine/src/animation/Animation.cpp
@@ -37,17 +37,19 @@ void Animation::AddFrame(float frameX, float frameY, float frameWidth, float fra
 	totalDuration = durationInSecs * 1000.0f;
 
 	Frame newFrame;
-	newFrame.frameCoords[0].u = frameX / textureWidth;
+	//Top right
+	newFrame.frameCoords[0].u = (frameX) / textureWidth;
 	newFrame.frameCoords[0].v = (frameY + frameHeight) / textureHeight;
-
-	newFrame.frameCoords[1].u = (frameX + frameWidth) / textureWidth;
-	newFrame.frameCoords[1].v = (frameY + frameHeight) / textureHeight;
-
-	newFrame.frameCoords[2].u = frameX / textureWidth;
+	//Bottom right
+	newFrame.frameCoords[1].u = (frameX) / textureWidth;
+	newFrame.frameCoords[1].v = frameY / textureHeight;
+	//Bottom left
+	newFrame.frameCoords[2].u = (frameX + frameWidth) / textureWidth;
 	newFrame.frameCoords[2].v = frameY / textureHeight;
-
+	//Top left
 	newFrame.frameCoords[3].u = (frameX + frameWidth) / textureWidth;
-	newFrame.frameCoords[3].v = frameY / textureHeight;
+	newFrame.frameCoords[3].v = (frameY + frameHeight) / textureHeight;
+	frames.push_back(newFrame);
 
 	frames.push_back(newFrame);
 }
